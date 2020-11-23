2772bb8fe23cc7a318054d0335a61e4ba7710a6b
faizulnavi <faizulnavi@yahoo.co.in>
2020-11-23 22:06:03 +0530

    Add css to design front page.

---
+++ OrderApplication.WebUI/Controllers/CartController.cs
@@ -0,0 +1,23 @@
+﻿using OrderApplication.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OrderApplication.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult Summary(Cart cart)
+        {
+            return PartialView(cart);
+        }
+    }
+}
