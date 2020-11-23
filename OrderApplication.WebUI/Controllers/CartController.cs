53e58fdee4abb89c802b158c5a2f2a715a4d902d
faizulnavi <faizulnavi@yahoo.co.in>
2020-11-23 22:21:44 +0530

    Created Cart controller and implemented business rules.

--- OrderApplication.WebUI/Controllers/CartController.cs
+++ OrderApplication.WebUI/Controllers/CartController.cs
@@ -1,4 +1,5 @@
 ﻿using OrderApplication.Domain.Entities;
+using OrderApplication.WebUI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,15 +10,78 @@ namespace OrderApplication.WebUI.Controllers
 {
     public class CartController : Controller
     {
+        private IProductRepository repository;
+        private IOrderProcessor orderProcessor;
         // GET: Cart
-        public ActionResult Index()
+        public ViewResult Index(Cart cart, string returnUrl)
         {
-            return View();
+            return View(
+            new CartIndexViewModel { Cart = cart, ReturnUrl = returnUrl });
         }
+
 
         public PartialViewResult Summary(Cart cart)
         {
             return PartialView(cart);
+        }
+
+        public ViewResult Checkout(Cart cart, ShippingDetails shippingDetails)
+        {
+
+            if (cart.Lines.Count() == 0)
+            {
+                ModelState.AddModelError("", "No Item added!");
+            }
+            //Rules rule = new Rules();
+            PackingSlip slip = new PackingSlip();
+            Membership membership = new Membership();
+            string cat = cart.Lines.Select(s => s.Product.Product_Category).FirstOrDefault();
+
+            string strView = "";
+            if (ModelState.IsValid)
+            {
+                if (cat == "Physical")
+                {
+                    slip.GeneratePackingSlip(cart, shippingDetails, cat, "No");
+                    cart.Clear();
+                    strView = "PhysicalView";
+                }
+
+                if (cat == "Book")
+                {
+                    slip.GeneratePackingSlip(cart, shippingDetails, cat, "No");
+                    cart.Clear();
+                    strView = "BookView";
+                }
+
+                if (cat == "Membership")
+                {
+                    string prod = cart.Lines.Select(s => s.Product.Product_Name).FirstOrDefault();
+                    string strMsg = "";
+                    if (prod == "Member_New")
+                    {
+                        membership.CreateMembership(prod);
+                        strMsg = "Congratulation: Your Membership has been activated";
+                        cart.Clear();
+                    }
+                    else
+                    {
+                        membership.UpdaradMembership(prod, 1);
+                        strMsg = "Congratulation: Your Membership has been updraded";
+                        cart.Clear();
+                    }
+                    orderProcessor.ProcessOrder(cart, shippingDetails, strMsg);
+                    strView = "Completed";
+                }
+
+
+
+                return View(strView);
+            }
+            else
+            {
+                return View(shippingDetails);
+            }
         }
     }
 }
